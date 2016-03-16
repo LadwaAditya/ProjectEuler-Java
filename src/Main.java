5b7dac1fe5d57a0995e955b35b01edd1f2f6aad6
Aditya Ladwa <ladwa.aditya@gmail.com>
2016-03-16 12:22:46 +0530

    fect : Created class for Multiple
    Created logic to find Mutiple of 3 and 5

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        Multiply multiply = new Multiply(1000);
+        multiply.displaySum();
     }
 }
--- src/Multiply.java
+++ src/Multiply.java
@@ -2,5 +2,17 @@
  * Created by Aditya on 16-Mar-16.
  */
 public class Multiply {
+    private long sum = 0;
 
+    public Multiply(int num) {
+        for (int i = 0; i < num; i++) {
+            if (i % 3 == 0 || i % 5 == 0)
+                this.sum += i;
+        }
+
+    }
+
+    public void displaySum() {
+        System.out.println("The sum of multiples is " + sum);
+    }
 }
