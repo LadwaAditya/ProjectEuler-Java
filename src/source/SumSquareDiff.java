d2d08c21373f444c787d5b2dd8f2b8bee46d196e
Aditya Ladwa <ladwa.aditya@gmail.com>
2016-03-21 22:51:00 +0530

    fect : Created class for Project 6 sum square difference

---
+++ src/source/SumSquareDiff.java
@@ -0,0 +1,25 @@
+package source;
+
+/**
+ * Sum square difference
+ * Problem 6
+ * The sum of the squares of the first ten natural numbers is,
+ * <p>
+ * 12 + 22 + ... + 102 = 385
+ * The square of the sum of the first ten natural numbers is,
+ * <p>
+ * (1 + 2 + ... + 10)2 = 552 = 3025
+ * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+ * <p>
+ * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+ * Created by Aditya on 21-Mar-16.
+ */
+public class SumSquareDiff {
+    private long sumSquareDiff = 0;
+    private long sumSquare = 0;
+    private long squareSum = 0;
+
+    public SumSquareDiff() {
+
+    }
+}
